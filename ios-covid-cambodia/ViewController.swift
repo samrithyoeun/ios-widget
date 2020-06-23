c21a46b20ce9be62225e200250e45d9bef790b18
sam yoeun <sam.yoeun@pathmazing.com>
2020-06-23 21:17:56 +0700

    Initial Commit

---
+++ ios-covid-cambodia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-covid-cambodia
+//
+//  Created by Samrith Yoeun on 6/23/20.
+//  Copyright © 2020 Sammi Yoeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
